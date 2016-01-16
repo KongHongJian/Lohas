1610884dab921eb784ee1ba628c07fd70a59aca0
孔宏建 <hongjian_kong@outlook.com>
2016-01-16 16:57:52 +0800

    new

---
+++ Lohas/Web/Lohas.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lohas.Web.Startup))]
+namespace Lohas.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
