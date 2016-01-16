016ad78d49884b51509fba90350e1dcc41f4128e
孔宏建 <hongjian_kong@outlook.com>
2016-01-16 16:08:07 +0800

    test

---
+++ Lohas/DataAccess/DataAccess/Class1.cs
@@ -0,0 +1,21 @@
+﻿//========================================================================
+// Copyright(C): Lohas
+//
+// CLR Version : 4.0.30319.42000
+// NameSpace : $rootnamespace$
+// FileName : Class1
+// Created by : Jerry (hongjian_kong@outlook.com) at 2016/1/16 16:07:23
+// Function :Class1  
+//================================================================
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class Class1
+    {
+    }
+}
---
+++ Lohas/Lohas.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lohas.Web.MvcApplication" Language="C#" %>
